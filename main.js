028c871942efb22037898723f33c4d3229e89c54
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-09-14 16:30:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 Webcam.set({
-    width:350,
+    width:310,
     height:300,
     image_format:'png',
-    png_quality:90
+    png_quality:90,
+    constraints:{
+        facingMode:"environment"
+    }
 });
 camera=document.getElementById("camera");
 Webcam.attach('#camera');
+
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'">';
     });
 }
 console.log('ml5 version',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/eMzCKY_Q0/',modelLoaded);
+classifier= ml5.imageClassifier('MobileNet',modelLoaded);
 function modelLoaded(){
-    console.log('model loaded');
+    console.log('model Loaded');
 }
 function check(){
-    img=document.getElementById('captured_image');
+    img=document.getElementById('capture_image');
     classifier.classify(img,gotResult);
 }
-function gotResult(error,results){
-    if(error){
+function gotResult(error, results){
+    if (error){
         console.error(error);
-    }else{
+    }
+    else{
         console.log(results);
-        document.getElementById("result_object_name").innerHTML=results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+        document.getElementById("object_name").innerHTML= results[0].label;
     }
 }
